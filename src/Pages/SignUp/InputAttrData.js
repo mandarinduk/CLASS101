ff99a8504d7eb655b3408da93eeb9668ccc0b8db
Moon-Sangho <skym887@gmail.com>
2020-10-06 21:11:25 +0900

    [12기 문상호] Add: 로그인 페이지 레이아웃 완성

---
+++ src/Pages/SignUp/InputAttrData.js
@@ -0,0 +1,34 @@
+const InputAttrData = [
+  {
+    label: "이름",
+    placeholder: "홍길동",
+    errorMessage: "이름을 입력해주세요",
+    name: "nameValue",
+  },
+  {
+    label: "이메일",
+    placeholder: "example@example.com",
+    errorMessage: "이메일을 입력해주세요",
+    name: "emailValue",
+  },
+  {
+    label: "휴대전화 번호",
+    placeholder: "-를 제외한 휴대폰 번호를 입력해주세요",
+    errorMessage: "휴대전화 번호를 입력해주세요",
+    name: "phoneValue",
+  },
+  {
+    label: "비밀번호 (8자 이상)",
+    placeholder: "********",
+    errorMessage: "패스워드를 입력해주세요",
+    name: "pwValue",
+  },
+  {
+    label: "비밀번호 확인",
+    placeholder: "********",
+    errorMessage: "패스워드를 확인해주세요",
+    name: "pwCheckValue",
+  },
+];
+
+export default InputAttrData;
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,7 +1,173 @@
-import React from "react";
+import React, { useState } from "react";
+import InputAttrData from "./InputAttrData";
+import styled from "styled-components";
 
 const SignUp = () => {
-  return <div></div>;
+  const [inputs, setInputs] = useState({
+    nameValue: "",
+    emailValue: "",
+    phoneValue: "",
+    pwValue: "",
+    pwCheckValue: "",
+  });
+
+  const { nameValue, emailValue, phoneValue, pwValue, pwCheckValue } = inputs;
+
+  const handleInput = (e) => {
+    console.log(e.target);
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const validation = () => {};
+
+  return (
+    <Wrapper>
+      <SignUpForm>
+        <Title>회원가입</Title>
+        {InputAttrData.map((data, index) => {
+          return (
+            <InputGroup key={index}>
+              <InputTitle>{data.label}</InputTitle>
+              <Input
+                placeholder={data.placeholder}
+                name={data.name}
+                value={Object.values(inputs)[index]}
+                onChange={handleInput}
+              />
+              <InputErrMsg>{data.errorMessage}</InputErrMsg>
+            </InputGroup>
+          );
+        })}
+        <CheckBoxGroup>
+          <CheckBox type="checkBox" />
+          <CheckContent>
+            <Span>이용약관</Span> 및 <Span>개인정보 처리방침</Span> 동의
+          </CheckContent>
+        </CheckBoxGroup>
+        <CheckBoxGroup>
+          <CheckBox type="checkBox" />
+          <CheckContent>이벤트 및 할인 소식 알림 동의 (선택)</CheckContent>
+        </CheckBoxGroup>
+        <Label>
+          ┗ 알림 동의 시 <Span red>3만원 쿠폰</Span> 즉시 발급!
+        </Label>
+        <Button orange>회원가입 하기</Button>
+        <Button>
+          <svg width="24" height="24" viewBox="0 0 24 24">
+            <path
+              fill="#1b1c1d"
+              fillRule="evenodd"
+              d="M12 4c-4.97 0-9 3.185-9 7.115 0 2.557 1.707 4.8 4.27 6.054-.188.702-.682 2.545-.78 2.94-.123.49.178.483.376.351.155-.103 2.466-1.675 3.464-2.353.541.08 1.1.123 1.67.123 4.97 0 9-3.186 9-7.115C21 7.185 16.97 4 12 4"
+            ></path>
+          </svg>
+          카카오로 5초 만에 시작하기
+        </Button>
+      </SignUpForm>
+    </Wrapper>
+  );
 };
 
 export default SignUp;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SignUpForm = styled.form`
+  max-width: 984px;
+`;
+
+const Title = styled.h1`
+  position: relative;
+  left: 0;
+  font-size: 30px;
+  font-weight: 600;
+  margin: 40px 0;
+`;
+
+const InputGroup = styled.div`
+  width: 468px;
+  margin-bottom: 20px;
+`;
+
+const InputTitle = styled.h2`
+  font-size: 14px;
+  margin-bottom: 4px;
+`;
+
+const Input = styled.input`
+  width: 468px;
+  height: 48px;
+  padding: 0 16px;
+  margin-bottom: 4px;
+  border: 1px solid #dde0e2;
+  border-radius: 3px;
+  outline: none;
+  &:focus {
+    box-shadow: 0 0 0 0.5px #000000;
+  }
+`;
+
+const InputErrMsg = styled.label`
+  font-size: 12px;
+  color: #ff5252;
+  margin-left: 4px;
+`;
+
+const CheckBoxGroup = styled.div`
+  display: flex;
+  margin-bottom: 4px;
+`;
+
+const CheckBox = styled.input`
+  display: block;
+  width: 18px;
+  height: 18px;
+`;
+
+const CheckContent = styled.span`
+  font-size: 14px;
+  margin: 6px 0 0 6px;
+`;
+
+const Span = styled.span`
+  color: ${(props) => (props.red ? "#fc3d46" : "#46a0c0")};
+  font-weight: bold;
+  text-decoration: ${(props) => (props.red ? "none" : "underline")};
+  cursor: pointer;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-size: 11px;
+  padding-left: 30px;
+  margin: 4px 0 25px 0;
+`;
+
+const Button = styled.button`
+  display: block;
+  width: 468px;
+  height: 48px;
+  margin: 8px 0;
+  font-size: 16px;
+  font-weight: bold;
+  outline: none;
+  border: 1px solid ${(props) => (props.orange ? "#ff922b" : "#ffe812")};
+  border-radius: 3px;
+  color: ${(props) => (props.orange ? "#ffffff" : "#000000")};
+  background: ${(props) => (props.orange ? "#ff922b" : "#ffe812")};
+  cursor: pointer;
+  &:hover {
+    filter: brightness(0.9);
+  }
+
+  svg {
+    vertical-align: middle;
+    margin-right: 5px;
+  }
+`;
